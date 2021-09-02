28703d61ccd9be1e8a1d7e91b5e0bf39d715559b
spb5732 <spb5732@psu.edu>
2021-09-02 14:53:28 -0400

    increment decrement change color

--- hello-world/src/HelloWorld.js
+++ hello-world/src/HelloWorld.js
@@ -15,6 +15,7 @@ export class HelloWorld extends LitElement {
     return {
       title: { type: String },
       counter: { type: Number },
+      oldNumber: {type: Number}
     };
   }
 
@@ -22,16 +23,43 @@ export class HelloWorld extends LitElement {
     super();
     this.title = 'Hey there';
     this.counter = 5;
+     
   }
 
   __increment() {
     this.counter += 1;
+    this.ChangeColor(this.counter);
   }
+
+  __decrement(){
+
+    if(this.counter>0){
+        this.counter -=1;
+        this.ChangeColor(this.counter);
+    }
+    
+    
+
+  }
+
+
+  colors = ["#FFC0CB","#EE82EE","#800080","#6A5ACD","#4B0082",
+"#FFA07A","#FF0000","	#8B0000","#FFA500","#FF8C00","#FF4500"];
+
+    ChangeColor(count) {
+      
+      document.body.style.backgroundColor = this.colors[count];
+
+    }
+  
+  
 
   render() {
     return html`
-      <h2>${this.title} Nr. ${this.counter}!</h2>
+      <h2 data-count=${this.counter}>${this.title} Nr. ${this.counter}!</h2>
       <button @click=${this.__increment}>increment</button>
+      <button @click=${this.__decrement}>decrement</button>
+
     `;
   }
 }
